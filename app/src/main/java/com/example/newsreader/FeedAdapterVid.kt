cebd5c32cc0cff59806f4335682f41c22089f834
ShubhamKumar2802 <56202964+ShubhamKumar2802@users.noreply.github.com>
2020-06-25 23:42:21 +0530

    OnClickListener for Video section

--- app/src/main/java/com/example/newsreader/FeedAdapterVid.kt
+++ app/src/main/java/com/example/newsreader/FeedAdapterVid.kt
@@ -1,16 +1,20 @@
 package com.example.newsreader
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 
 class ViewHolderVid(v: View) {
     val tvTitleVid: TextView = v.findViewById(R.id.tvTitleVid)
     val tvPublishDate: TextView = v.findViewById(R.id.tvPublishDateVid)
+    val tvLinkVid: TextView = v.findViewById(R.id.tvLinkVid)
 }
 
 class FeedAdapterVid(context: Context, val resourceVid: Int, val applicationVid: List<FeedEntry>) :
@@ -39,6 +43,17 @@ class FeedAdapterVid(context: Context, val resourceVid: Int, val applicationVid:
 
         viewHolder.tvTitleVid.text = currentVid.title
         viewHolder.tvPublishDate.text = currentVid.publishDate
+        viewHolder.tvLinkVid.text = currentVid.link
+        Log.d(TAG, "FeedAdapterVideo: link is ${currentVid.link}")
+
+        //OnclickListener for Video Feeds
+        view.setOnClickListener {
+            val intentVid = Intent().apply {
+                action = Intent.ACTION_VIEW
+                data = Uri.parse(currentVid.link)
+            }
+            startActivity(it.context, intentVid, null)
+        }
 
         return view
     }
